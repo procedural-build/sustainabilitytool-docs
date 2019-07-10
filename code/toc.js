731ad3154db29b2c6525d0cf7cf38a4d41b86212
Mark Pitman <32191263+pitmanmw@users.noreply.github.com>
2019-07-10 15:46:31 +0200

    Added trailing slash to all links

--- code/toc.js
+++ code/toc.js
@@ -17,7 +17,7 @@ const TOC = ({sections, _pages, _ID, _relativeURL}) => {
                         <nav key={i}
                              className={`toc__section toc__section--${i}${page._url === _pages[_ID]._url ? ' toc__section--active' : ''}`}>
                             <h2 className='toc__section__headline'><a
-                                href={_relativeURL(page._url, _ID)}>{page.title}</a></h2>
+                                href={`${_relativeURL(page._url, _ID)}/`}>{page.title}</a></h2>
 
                             <ul>
                                 {page.docs?
@@ -25,7 +25,7 @@ const TOC = ({sections, _pages, _ID, _relativeURL}) => {
                                         const sectionName = partial.replace('.md', '');
                                         return (
                                             <li key={i}>
-                                                <a href={`${_relativeURL(page._url, _ID)}/fdafds#${sectionName}`}>{SentenceCase(sectionName.replace(/-/g, ' '))}</a>
+                                                <a href={`${_relativeURL(page._url, _ID)}/#${sectionName}`}>{SentenceCase(sectionName.replace(/-/g, ' '))}</a>
                                             </li>
                                         );
                                     }): null
