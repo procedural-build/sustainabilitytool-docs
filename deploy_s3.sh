c9bb117c1cc1cbfc756969708556efb9c9b20806
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-09-13 16:39:51 +0200

    playing with jenkins-x pipeline #9

--- deploy_s3.sh
+++ deploy_s3.sh
@@ -10,7 +10,7 @@ export AWS_SECRET_ACCESS_KEY=$(eval $(jx get vault-config) && /builder/home/.jx/
 export AWS_ACCESS_KEY_ID=$(eval $(jx get vault-config) && /builder/home/.jx/bin/vault kv get secret/pipelines/aws | grep -P -o "[A-Z]{6}\d\w.*")
 export AWS_STORAGE_BUCKET_KEY=$(eval $(jx get vault-config) && /builder/home/.jx/bin/vault kv get secret/pipelines/aws | grep -P -o "[a-z]{5}-.*")
 
-echo "ENV VARS:" "$AWS_S3_REGION_NAME" "$AWS_S3_HOST" "$AWS_S3_USE_SSL"
+echo "ENV VARS:" "$AWS_S3_REGION_NAME" "$AWS_DEFAULT_REGION" "$AWS_S3_HOST" "$AWS_S3_USE_SSL"
 echo "SECRETS" "$AWS_SECRET_ACCESS_KEY" "$AWS_ACCESS_KEY_ID" "$AWS_STORAGE_BUCKET_KEY"
 echo "Uploading files to ${S3_BUNDLE_PATH}"
 
