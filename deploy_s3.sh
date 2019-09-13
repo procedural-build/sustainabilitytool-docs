40e49aa2ee11e584aedd530fd816077e895a0fa7
Christian Kongsgaard <kongsgaard.c@gmail.com>
2019-09-13 17:29:36 +0200

    playing with jenkins-x pipeline #11

--- deploy_s3.sh
+++ deploy_s3.sh
@@ -6,8 +6,8 @@ else
   export S3_BUNDLE_PATH='docs.sustainabilitytool.com/latest/'
 fi
 
-export AWS_SECRET_ACCESS_KEY=$(eval $(jx get vault-config) && /builder/home/.jx/bin/vault kv get secret/pipelines/aws | grep -P -o "\d\w{2}\d\w{8,}")
-export AWS_ACCESS_KEY_ID=$(eval $(jx get vault-config) && /builder/home/.jx/bin/vault kv get secret/pipelines/aws | grep -P -o "[A-Z]{6}\d\w.*")
+export AWS_SECRET_ACCESS_KEY=$(eval $(jx get vault-config) && /builder/home/.jx/bin/vault kv get secret/pipelines/aws | grep -P -o "\w{4}\d{2}\w{4}\+.*")
+export AWS_ACCESS_KEY_ID=$(eval $(jx get vault-config) && /builder/home/.jx/bin/vault kv get secret/pipelines/aws | grep -P -o "[A-Z]{6,}\d\w.*")
 export AWS_STORAGE_BUCKET_KEY=$(eval $(jx get vault-config) && /builder/home/.jx/bin/vault kv get secret/pipelines/aws | grep -P -o "[a-z]{5}-.*")
 
 echo "ENV VARS:" "$AWS_S3_REGION_NAME" "$AWS_REGION" "$AWS_S3_HOST" "$AWS_S3_USE_SSL"
