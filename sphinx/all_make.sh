6666506f572bafcbe644f06cfeef0d4763878b94
Mark Pitman <mark@ods-engineering.com>
2019-10-30 10:53:48 +0100

    Copied assets folder into build directory after build

--- sphinx/all_make.sh
+++ sphinx/all_make.sh
@@ -6,7 +6,7 @@ find ./source -name "*.rst" | xargs rm -v
 # Delete all folders except those starting with "_"
 find ./source/ -type d -maxdepth 1 -mindepth 1 -name "*" | grep -v "/_" | xargs rm -rvf
 
-# Copy the assetts folder into place
+# Copy the assets folder into place
 cp -r ../assets ./source/
 
 # Make the RST source files from markdown in the contents folder
@@ -14,3 +14,6 @@ cp -r ../assets ./source/
 
 # Compile from RST to HTML
 make clean && make html
+
+# Copy the assets folder into the build directory
+cp -r ../assets ./build/html
